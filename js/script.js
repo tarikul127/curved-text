2f14f43452e366cd4537d8acec575141952639ca
tarikul127 <158174361+tarikul127@users.noreply.github.com>
2024-09-01 17:02:40 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*==================== js ================  */
+
+
+
+
+
+
+
+/* ===================jq ===================*/
+$(document).ready(function(){
+
+ var arc = function (t)
+    {
+        return {
+            x: 500.0 + 300.0 * Math.cos(2.0 * Math.PI * t - 0.5 * Math.PI),
+            y: 400.0 + 300.0 * Math.sin(2.0 * Math.PI * t - 0.5 * Math.PI)
+        };
+    };
+
+    $('#text1').curvedText({
+        curve:    arc,
+        domain:   [-0.2, 0.2],
+        viewport: { x: 0.0, y: 0.0, width: 1000.0, height: 400.0 }
+    });
+
+
+
+
+});
+
+ 
